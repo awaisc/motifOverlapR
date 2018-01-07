a5e4dc8bf958dbb35c6924ffe3562a296493f008
awaisc <a1649239@student.adelaide.edu.au>
2018-01-08 03:47:08 +1030

    fixed path issues with conserved regions

---
+++ DataFiles/UnbiasedTFBSPrediction/ScriptForGeneratingTheIntearctionTrackFile.R
@@ -0,0 +1,34 @@
+# PromotersAssoicatedWithEnhancers%>%
+#   as.data.frame()%>%
+#   select(c("seqnames", "start", "end", "enhancer", "correlation", "FDR"))%>%
+#   separate(col=`enhancer`, into= c("seqnames2", "start2", "end2"))%>%tibble-> InteractionDataFrameCAGEExpression
+# 
+# SortedTable<-cbind.data.frame(InteractionDataFrameCAGEExpression$seqnames,
+#                               InteractionDataFrameCAGEExpression$start,
+#                               InteractionDataFrameCAGEExpression$end,
+#                               InteractionDataFrameCAGEExpression$seqnames2,
+#                               InteractionDataFrameCAGEExpression$start2,
+#                               InteractionDataFrameCAGEExpression$end2,
+#                               "Interaction",
+#                               InteractionDataFrameCAGEExpression$correlation*10)
+# 
+# write.table(SortedTable, sep = "\t", 
+#             append=FALSE,
+#             quote=FALSE,
+#             col.names = FALSE,
+#             row.names = FALSE,
+#             file= "/media/awais/NewDrivewho/UnbiasedTFBSPrediction/Unbiased TFBS Prediction/DataFiles/Interactions/Human/EnhancerPromoterAssoication.bedpe"
+#             )
+# EnhancerAndCAGEPRomoterInteraction<-makeGenomicInteractionsFromFile(fn = "/media/awais/NewDrivewho/UnbiasedTFBSPrediction/Unbiased TFBS Prediction/DataFiles/Interactions/Human/EnhancerPromoterAssoication.bedpe", type = "bedpe")
+# 
+ mcols(EnhancerAndCAGEPRomoterInteraction)<-cbind.data.frame(mcols(EnhancerAndCAGEPRomoterInteraction), "FDR" = InteractionDataFrameCAGEExpression$FDR)
+ 
+ saveRDS(object = EnhancerAndCAGEPRomoterInteraction, "DataFiles/Interactions/Human/EnhancerPromoterAssoicationRObject")
+# 
+# InteractionTrack<-InteractionTrack(EnhancerAndCAGEPRomoterInteraction)
+
+CageExpressionGenomicIntearctions <- readRDS(file = "DataFiles/Interactions/Human/EnhancerPromoterAssoicationRObject")
+
+IntearctionTrack<-CageExpressionGenomicIntearctions%>%InteractionTrack()
+
+plotTracks(IntearctionTrack, from= 20000000, to= 25000000, chromosome = "chrX")
